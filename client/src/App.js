ea32a6971c4b22960483b93e3a5231ecedbfa2c5
harshinvader <emails.harsha@gmail.com>
2020-04-23 00:05:48 +0530

    Added app logo and basic home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Nav from "./Nav";
+import About from "./Components/About/About";
+import Order from "./Components/Order/Order.js";
+import Home from "./Components/Home/Home.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />{" "}
+          <Route path="/order" exact component={Order} />{" "}
+          <Route path="/about" exact component={About} />{" "}
+          {/* <Route path="/order/:id" exact component={ItemDetail} /> */}{" "}
+        </Switch>{" "}
+      </div>{" "}
+    </Router>
   );
 }
 
