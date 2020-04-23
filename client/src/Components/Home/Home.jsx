adf242e737070a0d999811fa029de21537c1d4e2
harshinvader <emails.harsha@gmail.com>
2020-04-23 14:19:42 +0530

    Added login with google button

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -7,6 +7,19 @@ function App() {
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <h1 className="title">Universical</h1>
+      <div className="google-btn">
+        <div className="google-icon-wrapper">
+          <img
+            className="google-icon"
+            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+            alt="google icon"
+          />
+        </div>
+        <a className="btn-text" href="/auth/google">
+          Login with Google
+        </a>
+      </div>
+      <br />
       <a href="/api/event">/api/event</a>
       <a href="/api/event/add">/api/event/add</a>
     </header>
