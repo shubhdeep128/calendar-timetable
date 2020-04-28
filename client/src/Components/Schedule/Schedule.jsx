efbbbff7f347811151976899558508b28539d626
harshinvader <emails.harsha@gmail.com>
2020-04-28 19:50:00 +0530

    Added loading sign in /schedule

--- client/src/Components/Schedule/Schedule.jsx
+++ client/src/Components/Schedule/Schedule.jsx
@@ -5,33 +5,42 @@ import "./Schedule.css";
 export default class Schedule extends Component {
   state = {
     data: [],
+    loaded: false,
   };
   componentDidMount() {
     axios
       .get("/api/event/")
       .then((res) => {
         console.log(res);
-        this.setState({ data: res.data.event });
+        this.setState({ data: res.data.event, loaded: true });
       })
       .catch((error) => {
         console.log("ERROR LOADING DATA");
         console.log(error);
       });
   }
+  loading = () => (
+    <div className={this.state.loaded ? "ajax-loaded" : "ajax-loading"}>
+      LOADING...
+    </div>
+    /* The class ajax-loaded -- you guessed it, hides the div */
+    /* The thing to always keep in mind -- when state changes, things happen! */
+  );
   render() {
     return (
       <div>
         <h1>Upcoming Events</h1>
+        {this.loading()}
         <div className="upComing">
           {this.state.data.map((data) => (
             <div className="upComingChild">
               <h1>{data.summary}</h1>
               <div className="details">
                 <div className="startDate">
-                  From: {data.start.dateTime.substring(0, 10)}
+                  From: {data.start.dateTime.substring(0, 10).slice(-4)}
                 </div>
                 <div className="endDate">
-                  To: {data.end.dateTime.substring(0, 10)}
+                  To: {data.end.dateTime.substring(0, 10).slice(-4)}
                 </div>
               </div>
             </div>
